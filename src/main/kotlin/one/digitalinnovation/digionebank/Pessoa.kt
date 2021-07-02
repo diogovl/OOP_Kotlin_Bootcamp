8a3b3e9d3a42aebc62a084cb673ea9ac8a954333
diogovl <ecompdiogo+git@gmail.com>
2021-07-02 14:34:11 -0300

    Updated to apply the private modifier to set funciton of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,17 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Diogo"
     var cpf: String = "123.123.123.11"
-
-    inner class Endereco {
-        var rua: String = "Rua Teste"
-    }
+    private set
 }
 
 fun main() {
-    val Diogo = Pessoa()
+    val diogo = Pessoa()
+    println(diogo.nome)
+    println(diogo.cpf)
 
-    println(Diogo.nome)
-    println(Diogo.cpf)
 
-    println(Diogo.Endereco().rua)
 }
